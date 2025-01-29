b865ce3e0eaaceb53b7966c2f4b8fb1f4417222a
filipe <filipe.candido.rodrigues@gmail.com>
2025-01-28 23:06:33 -0300

    projeto tabuada finalizado

--- projetos-utilizando html-css-javascript/projeto-tabuada/js/script.js
+++ projetos-utilizando html-css-javascript/projeto-tabuada/js/script.js
@@ -1,15 +1,57 @@
 //seleção de elemntos 
 
 const multiplicationForm = document.querySelector('#multiplication-form')
-const numberInput =document.querySelector('#number')
+const numberInput = document.querySelector('#number')
 const multiplicationInput = document.querySelector('#multiplicator')
 
+const multiplicationTable = document.querySelector('#multiplication-operations')
+
+const multiplicationTitle = document.querySelector('#multiplication-title span')
 
 //função
 
-function multiplicationTabuada (){
+const createTable = (number, multiplicatorNumber) => {  //arrow function
 
+        multiplicationTable.innerHTML =""  //limpando html para liberar os valores
+
+        for( i =0; i <= multiplicatorNumber; i++){
+
+            const result = number*i
+
+            const template = `<div class="row">
+            
+                    <div class ="operation">${number} x ${i} = </div>
+                    <div class ="result">${result}</div>
+                </div>`
+
+            const parser = new DOMParser()
+            const htmlTemplate = parser.parseFromString(template, "text/html")
+
+            const row = htmlTemplate.querySelector('.row')
+
+            multiplicationTable.appendChild(row)
+        }
+
+
+        multiplicationTitle.innerText =number
 }
 
 
 //Eventos
+
+
+multiplicationForm.addEventListener("submit",(e)=>  {
+
+
+        e.preventDefault()
+
+        const multiplicationNumber = +numberInput.value
+        const multiplicatorNumber = +multiplicationInput.value
+
+        if(!multiplicationNumber || !multiplicatorNumber)  // se algum dos campos não forem preenchidos
+            return
+            
+        
+        createTable(multiplicationNumber, multiplicatorNumber)
+    
+})
