b0869705843f1c58dc09210b8c4d43ddd064c223
filipe <filipe.candido.rodrigues@gmail.com>
2025-01-28 22:26:49 -0300

    commit initial projeto tabuada

---
+++ projetos-utilizando html-css-javascript/projeto-tabuada/js/script.js
@@ -0,0 +1,15 @@
+//seleção de elemntos 
+
+const multiplicationForm = document.querySelector('#multiplication-form')
+const numberInput =document.querySelector('#number')
+const multiplicationInput = document.querySelector('#multiplicator')
+
+
+//função
+
+function multiplicationTabuada (){
+
+}
+
+
+//Eventos
